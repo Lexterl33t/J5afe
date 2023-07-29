e0b204f468003d3a606f05e819cbd04d29d82e3d
Lexterl33t <paraghostsociety@gmail.com>
2023-07-29 02:51:23 +0200

    add readme

--- index.js
+++ index.js
@@ -89,30 +89,4 @@ class BreakPointAST extends acorn.Parser {
 	}
 }
 
-var code = `
-let lol = 10;
 
-console.log(lol)
-
-if (lol == 10 + 10) {
-	console.log("ok")
-} else {
-	console.log("nop")
-}
-`	
-
-let br = new BreakPointAST(code)
-
-br.addNodeBreakPoint("CallExpression", function(node) {
-	console.log(node)
-})
-
-br.addNodeBreakPoint("Literal", function(node) {
-	console.log(node.value)
-})
-
-br.addNodeBreakPoint("BinaryExpression", function(node) {
-	console.log(node)
-})
-
-br.walk()
