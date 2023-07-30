3c60f8886ff063fe92ccd299e7989cc5492e2153
Lexterl33t <paraghostsociety@gmail.com>
2023-07-30 02:35:34 +0200

    update index

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 import BreakPointAST from './src/BreakPointAST.js'
 
-function generate_string(size) {
-    return (Math.random() + 1).toString(36).substring(size);
+
+export {
+    BreakPointAST as default
 }
-
-let code = `
-    let d = (10 + 10) + 5 * 6 + 6 + 30 * 1337 - 10;
-    let c = d * 13 + 444 - 3 + 188
-    let dd = "lol"
-`
-
-let br = new BreakPointAST(code)
-
-br.addNodeBreakPoint("BinaryExpression", function(ctx, node, builder, obfu) {
-    ctx.replaceExpression(node, obfu.generate_mba(node))
-})
-
-br.walk()
-console.log(br.ast)
-
 
 //console.log(br.ast.body[0].declarations[0])
